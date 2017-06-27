23228851348149b617f1fc12b49cf7bf0fd958e1
alexander <mosesad91@gmail.com>
2017-06-27 00:31:52 +0000

    Decided to go with networkx for graph viz
    Just playing around with some options for integrating pypower with some visualization

--- model.py
+++ model.py
@@ -1,7 +1,27 @@
-import numpy
+import numpy as np
 import tensorflow as tf
-import scipy
-import sklearn
-import pandas
+import scipy as sp
+import sklearn as skl
+import pandas as pd
+import pypower.api as pp
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Load pypower case and run power flow analysis
+c6 = pp.loadcase(pp.case6ww(), return_as_obj=True, expect_gencost=False, expect_areas=False)
+result = pp.runpf(c6)
+
+# Develop some graph visualization stuff
+G = nx.Graph()
+G.add_node(1, {'size': 10, 'color': 'b'})
+G.add_node(2, {'size': 5, 'color': 'g'})
+G.add_node(3, {'size': 13, 'color': 'r'})
+G.add_edge(1, 2, {'length': 2})
+G.add_edge(1, 3, {'length': 1})
+G.add_edge(2, 3, {'length': 3})
+
+nx.draw(G)
+plt.show()
+
 
 
