a2ce6decd4d8811a3778252d0f4a16f5f510956d
alexander <mosesad91@gmail.com>
2017-09-05 19:31:27 +0000

    Close to finishing objective function.  Needs troubleshooting.
    There is a problem with running power flow on an optimal power flow
    result.  This is preventing me from using the distribute slack function
    between steps.  Next will be to learn best restoration order based on
    the objective function.

--- distribute_slack.py
+++ distribute_slack.py
@@ -1,32 +1,32 @@
 from copy import deepcopy
 import numpy as np
 from oct2py import octave
+from config import mp_opt
 
 
-def distribute_slack(test_case, slack_ind, droop_constants, converge_options, mp_options):
+def distribute_slack(case, slack_ind, droop_constants, converge_options):
     """
     Redistributes the added slack generation to all generators according to participation factors
 
-    :param test_case: Matpower test case (containing faulted lines)
+    :param case: Matpower test case (containing faulted lines)
     :param slack_ind: Index to the slack bus
     :param droop_constants: percent droop of each generator
     :param converge_options: convergence options
-    :param mp_options: Matpower options
     :return test_result_dist: test case result (faulted lines) with adjusted generation profile
     """
 
     # Copy of test case worked with
-    test_case_dist = deepcopy(test_case)
+    case_distr = deepcopy(case)
 
     # Original power generated at slack bus
-    base_slack = test_case['gen'][slack_ind, 1]
+    base_slack = case['gen'][slack_ind, 1]
 
     # Normalize droop
-    droop_weighted = droop_constants*test_case['gen'][:, 6]
+    droop_weighted = droop_constants*case['gen'][:, 6]
     droop_norm = droop_weighted/np.sum(droop_weighted).reshape((-1, 1))
 
     # Calculate added slack bus load
-    test_result_dist = octave.runpf(test_case_dist, mp_options)
+    test_result_dist = octave.runpf(case_distr, mp_opt)
 
     # Return if power flow does not converge
     if test_result_dist['success'] == 0:
@@ -40,13 +40,13 @@ def distribute_slack(test_case, slack_ind, droop_constants, converge_options, mp
     while slack_diff > converge_options['tolerance'] and count <= converge_options['max iteration']:
 
         # Redistribute slack
-        test_case_dist['gen'][:, 1] = test_case_dist['gen'][:, 1] + droop_norm*slack_diff
+        case_distr['gen'][:, 1] = case_distr['gen'][:, 1] + droop_norm*slack_diff
 
         # Run power flow
-        test_result_dist = octave.runpf(test_case_dist, mp_options)
+        test_result_dist = octave.runpf(case_distr, mp_opt)
 
         # Change in slack output before and after
-        slack_diff = test_result_dist['gen'][slack_ind, 1] - test_case_dist['gen'][slack_ind, 1]
+        slack_diff = test_result_dist['gen'][slack_ind, 1] - case_distr['gen'][slack_ind, 1]
 
         count += 1
 
